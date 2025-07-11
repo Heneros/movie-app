abb445e02dc85e974747d29846706f7c0c08dbf7
Heneros <punk60826@gmail.com>
2025-07-11 12:05:52 +0300

    vercel v7

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,6 @@
-import 'tsconfig-paths/register';
+// import 'tsconfig-paths/register';
+require('tsconfig-paths/register');
+
 
 import { HttpAdapterHost, NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
